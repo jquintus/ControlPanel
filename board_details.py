519f46a77f260be90d50cdf46f1d5cf77b97dbb1
Josh Quintus <josh.quintus@gmail.com>
2024-12-02 22:59:36 -0500

    Clean up code

--- board_details.py
+++ board_details.py
@@ -4,27 +4,22 @@ import microcontroller
 
 class BoardDetails:
     def __init__(self):
-        pass
+        self.os_version = os.uname().version
+        self.machine = os.uname().machine
+        self.board_id = board.board_id
+
+        unique_id = microcontroller.cpu.uid
+        self.unique_id = ":".join("{:02x}".format(byte) for byte in unique_id)
+
+        #self.cpu_name = microcontroller.cpu.name
+        self.cpu_frequency = microcontroller.cpu.frequency
 
     def get_details(self):
         lines = []
-        lines.append(f"CircuitPython Version: {os.uname().version}")
-        lines.append(f"Machine Info: {os.uname().machine}")
-        lines.append(f"Board: {board.board_id}")
-
-        # Get the unique ID of the microcontroller
-        unique_id = microcontroller.cpu.uid
-        unique_id_str = ":".join("{:02x}".format(byte) for byte in unique_id)
-
-        # Get the microcontroller name
-        #cpu_name = microcontroller.cpu.name
-
-        # Get the frequency of the CPU
-        cpu_frequency = microcontroller.cpu.frequency
-
-        # Print board-specific information
-        lines.append(f"Microcontroller Unique ID: {unique_id_str}")
-        #print("CPU Name:", cpu_name)
-        lines.append(f"CPU Frequency (Hz): {cpu_frequency}")
+        lines.append(f"CircuitPython Version: {self.os_version}")
+        lines.append(f"Machine Info: {self.machine}")
+        lines.append(f"Board: {self.board_id}")
+        lines.append(f"Microcontroller Unique ID: {self.unique_id}")
+        lines.append(f"CPU Frequency (Hz): {self.cpu_frequency}")
 
         return lines
